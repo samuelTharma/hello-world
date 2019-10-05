cbaaf276c6a97056e3b7178fd605c885e768ce2a
Tharma <satheeskumar@gmail.com>
2019-10-05 08:53:19 +0100

    my first commit

---
+++ src/uk/test/git/MyFirstGit.java
@@ -0,0 +1,15 @@
+package uk.test.git;
+
+public class MyFirstGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		printHi();
+
+	}
+	private static void printHi()
+	{
+		System.out.println("hello git ...");
+	}
+
+}
